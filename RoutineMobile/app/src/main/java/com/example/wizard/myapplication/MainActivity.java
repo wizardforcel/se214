380dcf1312c28b435f32c598e12e65ab709669ec
wizard <wizard.z@qq.com>
2014-07-21 12:07:55 +0800

    7.21 am

--- RoutineMobile/app/src/main/java/com/example/wizard/myapplication/MainActivity.java
+++ RoutineMobile/app/src/main/java/com/example/wizard/myapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.wizard.myapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
+import android.view.View;
+import android.app.*;
 import android.widget.*;
 import java.lang.*;
 import java.util.Calendar;
@@ -16,10 +20,61 @@ public class MainActivity extends ActionBarActivity
     private GridView CalendarTable;
     private Button SettingButton;
 
+    AlertDialog DateDialog;
+    private EditText YearText;
+    private EditText MonthText;
+
     private void InitViews()
     {
         CalendarTable = (GridView)findViewById(R.id.CalendarTable);
+
         SettingButton = (Button)findViewById(R.id.SettingButton);
+        SettingButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            { SettingButton_OnClick(view); }
+        });
+
+        View datedialogview = LayoutInflater.from(this).inflate(R.layout.date_dialog, null);
+        DateDialog = new AlertDialog.Builder(this).setTitle("请输入日期")
+                .setView(datedialogview)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    { DateDialogOKButton_OnClick(dialogInterface, i); }
+                })
+                .setNegativeButton("取消", null).create();
+        YearText = (EditText)datedialogview.findViewById(R.id.YearText);
+        MonthText = (EditText)datedialogview.findViewById((R.id.MonthText));
+    }
+
+    private void DateDialogOKButton_OnClick(DialogInterface dialogInterface, int i)
+    {
+        String yearstr = YearText.getText().toString();
+        int year = yearstr.equals("")? 0: Integer.parseInt(yearstr);
+        if(year < 1900 || year > 2999)
+        {
+            Toast.makeText(this, "请输入正确的年份！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String monthstr = MonthText.getText().toString();
+        int month = monthstr.equals("")? 0: Integer.parseInt(monthstr);
+        if(month < 1 || month > 12)
+        {
+            Toast.makeText(this, "请输入正确的月份！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ShowCalendar(year, month);
+    }
+
+
+    private void SettingButton_OnClick(View view)
+    {
+        DateDialog.show();
     }
 
     @Override
@@ -33,6 +88,9 @@ public class MainActivity extends ActionBarActivity
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH) + 1;
         ShowCalendar(year, month);
+
+        YearText.setText(String.valueOf(year));
+        MonthText.setText(String.valueOf(month));
     }
 
     private void ShowCalendar(int year, int month)
